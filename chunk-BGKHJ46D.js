599f65f7badaa416bbe03239eb93abf7e413c7d4
Dom-21 <130161618+Dom-21@users.noreply.github.com>
2026-05-28 06:30:26 +0000

    Deploying to gh-pages from @ Dom-21/dom-library-core@67adcaef3d2c81007c5e11ca71c7e8ecdda45f40 🚀

---
+++ chunk-BGKHJ46D.js
@@ -0,0 +1,1 @@
+import{H as r,R as o,aa as s,i as n}from"./chunk-Z2TQCWLG.js";function x(t){t||(t=o(s));let i=new n(e=>{if(t.destroyed){e.next();return}return t.onDestroy(e.next.bind(e))});return e=>e.pipe(r(i))}export{x as a};
