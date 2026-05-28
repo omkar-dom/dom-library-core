26fd26625a4047172cdb63cec0f3b5e72470dcdf
Dom-21 <130161618+Dom-21@users.noreply.github.com>
2026-05-28 06:38:22 +0000

    Deploying to gh-pages from @ Dom-21/dom-library-core@d0942c5bb6d9c8eb39588bad51deeff4d510434d 🚀

---
+++ chunk-RKVLS5CH.js
@@ -0,0 +1,1 @@
+import{H as r,R as o,aa as s,i as n}from"./chunk-R5WYVA5Z.js";function x(t){t||(t=o(s));let i=new n(e=>{if(t.destroyed){e.next();return}return t.onDestroy(e.next.bind(e))});return e=>e.pipe(r(i))}export{x as a};
